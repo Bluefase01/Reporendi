8e8155cc10180c850ae8e05f415049d9d2d9dee9
Rendi <159239226+Bluefase01@users.noreply.github.com>
2024-06-11 15:36:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web profile</title>
+</head>
+<body>
+<div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card p-0">
+                    <div class="card-image">
+                        <img src="img/3.jpeg"
+                            alt="">
+                            <link rel="stylesheet" href="style.css">
+                            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+                            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+                    </div>  
+                    <div class="card-content d-flex flex-column align-items-center">
+                        <h4 class="pt-2">SomeOne Famous</h4>
+                        <h5>Creative Desinger</h5>
+
+                        <ul class="social-icons d-flex justify-content-center">
+                            <li style="--i:1">
+                                <a href="#">
+                                    <span class="fab fa-facebook"></span>
+                                </a>
+                            </li>
+                            <li style="--i:2">
+                                <a href="#">
+                                    <span class="fab fa-twitter"></span>
+                                </a>
+                            </li>
+                            <li style="--i:3">
+                                <a href="#">
+                                    <span class="fab fa-instagram"></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card p-0">
+                    <div class="card-image">
+                        <img src="img/1.jpeg"
+                            alt="">
+                    </div>
+                    <div class="card-content d-flex flex-column align-items-center">
+                        <h4 class="pt-2">SomeOne Famous</h4>
+                        <h5>Creative Desinger</h5>
+
+                        <ul class="social-icons d-flex justify-content-center">
+                            <li style="--i:1">
+                                <a href="#">
+                                    <span class="fab fa-facebook"></span>
+                                </a>
+                            </li>
+                            <li style="--i:2">
+                                <a href="#">
+                                    <span class="fab fa-twitter"></span>
+                                </a>
+                            </li>
+                            <li style="--i:3">
+                                <a href="#">
+                                    <span class="fab fa-instagram"></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card p-0">
+                    <div class="card-image">
+                        <img src="img/2.jpg"
+                            alt="">
+                    </div>
+                    <div class="card-content d-flex flex-column align-items-center">
+                        <h4 class="pt-2">SomeOne Famous</h4>
+                        <h5>Creative Desinger</h5>
+
+                        <ul class="social-icons d-flex justify-content-center">
+                            <li style="--i:1">
+                                <a href="#">
+                                    <span class="fab fa-facebook"></span>
+                                </a>
+                            </li>
+                            <li style="--i:2">
+                                <a href="#">
+                                    <span class="fab fa-twitter"></span>
+                                </a>
+                            </li>
+                            <li style="--i:3">
+                                <a href="#">
+                                    <span class="fab fa-instagram"></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
